c32c2b819f55ae12b3620fec6b086bdf60cd5ef6
Raquel Amaral <raquelsamaral@icloud.com>
2020-09-24 14:16:12 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        fs.readFile('./public/index.html', 'UTF-8', (err, data) => {
+            if(err){
+                res.statusCode = 500;
+                res.end(err.message);
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.write(data);
+                res.end();
+            }
+        })
+    
+    } else if(req.url.match('\.css$')){
+        var cssPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+        res.write('Content-Type', 'text/css');
+        fileStream.pipe(res);
+    
+    } else if(req.url.match('\.png$')){
+        var imagePath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.write('Content-Type', 'image/png');
+        fileStream.pipe(res);
+    } else {
+        res.statusCode = 404;
+        res.end('Página não encontrada');
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+});
